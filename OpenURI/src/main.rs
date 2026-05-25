71b21c2d0930ff62c825f73708ae654c6ccb87c6
Tobias Bengfort <tobias.bengfort@posteo.de>
2026-05-25 12:06:06 +0200

    [OpenURI] port to rust

---
+++ OpenURI/src/main.rs
@@ -0,0 +1,83 @@
+use anyhow::{anyhow, Context, Result};
+use gio;
+use gio::prelude::FileExt;
+use libc::pid_t;
+use nix::fcntl::{open, openat2, OFlag, OpenHow, ResolveFlag};
+use nix::poll::{poll, PollFd, PollFlags, PollTimeout};
+use nix::sys::socket::{getsockopt, sockopt};
+use nix::sys::stat::{fstat, FileStat, Mode};
+use std::io::{Read, Write};
+use std::os::fd::{AsFd, FromRawFd, OwnedFd};
+use std::os::unix::io::RawFd;
+use std::os::unix::net::UnixStream;
+use std::path::{Path, PathBuf};
+
+const MAX_URI_LENGTH: usize = 1024;
+const SOCK_FD: RawFd = 3;
+
+fn check_running(pidfd: &OwnedFd) -> Result<()> {
+    let mut pollfds = [PollFd::new(pidfd.as_fd(), PollFlags::POLLIN)];
+    let nready = poll(&mut pollfds, PollTimeout::ZERO)?;
+    if nready != 0 {
+        return Err(anyhow!("sender no longer available"));
+    }
+    return Ok(());
+}
+
+fn stat_at_root(path: &Path, root: &Path) -> Result<FileStat> {
+    let root_fd = open(root, OFlag::O_RDONLY | OFlag::O_DIRECTORY, Mode::empty())?;
+
+    let how = OpenHow::new();
+    how.flags(OFlag::O_RDONLY);
+    how.resolve(ResolveFlag::RESOLVE_IN_ROOT);
+    let fd = openat2(root_fd, path, how)?;
+
+    let st = fstat(fd)?;
+    return Ok(st);
+}
+
+fn same_on_host(path: &Path, pid: pid_t, pidfd: OwnedFd) -> Result<()> {
+    let mut sender_root = PathBuf::from("/proc");
+    sender_root.push(pid.to_string());
+    sender_root.push("root");
+
+    let st_sender = stat_at_root(path, sender_root.as_path())?;
+    let st_host = stat_at_root(path, Path::new("/"))?;
+
+    check_running(&pidfd)?;
+
+    if st_sender.st_dev == st_host.st_dev && st_sender.st_ino == st_host.st_ino {
+        return Ok(());
+    } else {
+        return Err(anyhow!("Not the same file on the host"));
+    }
+}
+
+fn main() -> Result<()> {
+    let mut stream = unsafe { UnixStream::from_raw_fd(SOCK_FD) };
+
+    let mut buf = [0u8; MAX_URI_LENGTH];
+    let n = stream.read(&mut buf)?;
+    let mut uri = String::from_utf8(buf[..n - 1].to_vec())?;
+
+    if uri.starts_with('/') {
+        uri = gio::File::for_path(uri).uri().to_string();
+    }
+
+    if uri.starts_with("file://") {
+        let pid = getsockopt(&stream, sockopt::PeerCredentials)?.pid();
+        let pidfd = getsockopt(&stream, sockopt::PeerPidfd)?;
+        let path = gio::File::for_uri(&uri)
+            .path()
+            .context("file has no local path")?;
+        same_on_host(&path, pid, pidfd)?;
+    }
+
+    return match gio::AppInfo::launch_default_for_uri(&uri, gio::AppLaunchContext::NONE) {
+        Ok(()) => Ok(()),
+        Err(e) => {
+            stream.write_all(e.message().as_bytes())?;
+            return Err(e.into());
+        }
+    };
+}
